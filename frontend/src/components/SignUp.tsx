83e06e8eed8452ba0fefa4042b0c14b90e355642
ziyuanchong <ziyuanchong@gmail.com>
2024-06-20 17:18:25 +0800

    signup change

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -6,9 +6,15 @@ const SignUp: React.FC = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [message, setMessage] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
 
   const handleSignUp = async (e: React.FormEvent) => {
     e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage("Passwords don't match. Please re-enter.");
+      return;
+    }
+
     const { user, error } = await signUp(email, password);
     if (error) {
       setMessage(`Error: ${error.message}`);
@@ -18,18 +24,30 @@ const SignUp: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSignUp}>
-      <div>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+    <div className='background'>
+      <div className='header'>
+        <h1>EzTravel</h1>
       </div>
-      <div>
+      <div className='container'>
+        <form onSubmit={handleSignUp}>
+          <h1>Travel With Us!</h1>
+          <div className='input-box'>
+            <label>Email:</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          </div>
+        <div className='input-box'>
         <label>Password:</label>
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
       </div>
+      <div className='input-box'>
+            <label>Confirm Password:</label>
+            <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
+          </div>
       <button type="submit">Sign Up</button>
       <div>{message}</div>
-    </form>
+      </form>
+      </div>
+    </div>
   );
 };
 
