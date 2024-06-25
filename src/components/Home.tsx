e2a607878a62773e272488df17a1bd6b61b69f54
ziyuanchong <ziyuanchong@gmail.com>
2024-06-25 23:13:54 +0800

    googlemap changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import supabase from "../supabaseClient";
 import WeeklySchedule from "./WeeklySchedule";
+import Googlemaps from "./Googlemaps";
 
 function Home() {
   return <div><WeeklySchedule /></div>;
+  
 }
 
 export default Home;
