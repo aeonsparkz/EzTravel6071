ddef81cd4f6de2ece7f2e7a2efd863e0db18b53a
ziyuanchong <ziyuanchong@gmail.com>
2024-06-26 23:07:13 +0800

    add location

--- src/components/AddMeetingForm.tsx
+++ src/components/AddMeetingForm.tsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import Googlemaps from './Googlemaps';
 
 interface AddMeetingFormProps {
   userId: string;
   onAddMeeting: (date: string, time: string, description: string) => void;
 }
 
+
 const AddMeetingForm: React.FC<AddMeetingFormProps> = ({ userId, onAddMeeting }) => {
   const [date, setDate] = useState<string>('');
   const [time, setTime] = useState<string>('');
   const [description, setDescription] = useState<string>('');
+  const [popUp, setPopUp] = useState(false);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -50,6 +53,10 @@ const AddMeetingForm: React.FC<AddMeetingFormProps> = ({ userId, onAddMeeting })
           required
         />
       </div>
+      <div>
+          <button onClick={() => setPopUp(true)}>Add Location</button>
+          <Googlemaps trigger={popUp} setTrigger={setPopUp} />
+        </div>
       <button type="submit">Add Meeting</button>
     </form>
   );
